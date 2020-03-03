09f1991d7b4bd199fffcb93bc5b677b98d9ad895
Haris <haristopic@gmail.com>
2020-03-03 23:30:53 +0100

    uradjeni avatari, inputi, napravljen rootScope.

--- angularJS.js
+++ angularJS.js
@@ -1,5 +1,23 @@
 var varijabla = angular
                 .module("modul",[])
+                /*.controller("myCtrl", function($scope, $compile) {
+                    $scope.name = ' BILAL';
+                    $scope.test = function() {
+                        var element = angular.element($('#proba'));
+                        element.html('<p>HARIS{{name}}</p>');
+                        $compile(element)($scope);
+                    };
+                })*/
+                .run(function($rootScope) {
+                    $rootScope.daLiJeOznacenPrviAvatar=false
+                    $rootScope.vrijPrvogAvatara=""
+                    $rootScope.daLiJeOznacenDrugiAvatar=false
+                    $rootScope.vrijDrugogAvatara=""
+
+                    $rootScope.prvoIme=""
+                    $rootScope.drugoIme=""
+                })
+
                 .controller("kontroler",function($scope){
                     var right1=0;
                     var right2=-500;
@@ -56,16 +74,34 @@ var varijabla = angular
                     }
                 })
 
-                .controller("kontroler2",function($scope){
+                .controller("kontroler2",function($scope,$rootScope){
                     var hoverovano=[hoverovano0=false,hoverovano1=false,hoverovano2=false,hoverovano3=false,hoverovano4=false,hoverovano5=false,hoverovano6=false,hoverovano7=false,hoverovano8=false,hoverovano9=false,hoverovano10=false]
+                    var divovi=["#nulti","#prvi","#drugi","#treci","#cetvrti","#peti","#sesti","#sedmi","#osmi","#deveti",]
+                    var divovi2=["#nulti0","#prvi1","#drugi2","#treci3","#cetvrti4","#peti5","#sesti6","#sedmi7","#osmi8","#deveti9",]
+                    var slike=["PUBG.jpg","1.jpg","2.png","3.jpg","4.png","5.png","6.jpg","7.png","8.png","9.jpg"]
                     var daLiJeHoverovano=false
-                    $scope.hoveruj=function(redniBroj)
+                    $scope.hoveruj=function(redniBroj,redniBroj2)
                     {
                         if(daLiJeHoverovano==false)
                         {
                             for (var i=0;i<hoverovano.length;i++) {
                                 if(i==redniBroj)
                                 {
+                                    if(redniBroj2<10)
+                                    {
+                                        var element=angular.element($(divovi[i]))
+                                        element.html("<img src='checkSign.png'>")
+                                        $rootScope.daLiJeOznacenPrviAvatar=true
+                                        $rootScope.vrijPrvogAvatara=slike[i]
+                                    }
+                                    else
+                                    {
+                                        var element=angular.element($(divovi2[i]))
+                                        element.html("<img src='checkSign.png'>")
+                                        $rootScope.daLiJeOznacenDrugiAvatar=true
+                                        $rootScope.vrijDrugogAvatara=slike[i]
+                                    }
+
                                     daLiJeHoverovano=true
                                     hoverovano[i]=true
                                     return {"opacity":".25"}
@@ -75,6 +111,21 @@ var varijabla = angular
 
                         else if(hoverovano[redniBroj]==true)
                         {
+                            if(redniBroj2<10)
+                            {
+                                var element=angular.element($(divovi[redniBroj]))
+                                element.html("")
+                                $rootScope.daLiJeOznacenPrviAvatar=false
+                                $rootScope.vrijPrvogAvatara=""
+                            }
+                            else
+                            {
+                                var element=angular.element($(divovi2[redniBroj]))
+                                element.html("")
+                                $rootScope.daLiJeOznacenDrugiAvatar=false
+                                $rootScope.vrijDrugogAvatara=""
+                            }
+
                             hoverovano[redniBroj]=false
                             daLiJeHoverovano=false
                             return {"opacity":"1"}
@@ -82,26 +133,50 @@ var varijabla = angular
                     }
                 })
 
-                .controller("kontroler3",function($scope){
+                .controller("kontroler3",function($scope,$timeout){
                     $scope.player1=""
                     $scope.player2=""
 
                     $scope.pokreni=function(player1,player2){
                         if(player1!="" && player2!="")
                         {
+                            $rootScope.prvoIme=player1;
+                            $rootScope.drugoIme=player2;
                             $scope.stilEkrana={
-                                "transition":"1s",
-                                "filter":"brightness(.0)",
-                                "background":"black"
+                                "transition":"3s",
+                                "background":"black",
+                                "filter":"brightness(.0)"
                             }
-                            //Popraviti ovdje bug;
-                            setTimeout(function(){
+                            var stilEkrana2=$timeout(callTimeout,3000)
+
+                            function callTimeout(){
+                                $scope.sakrij={"display":"none"}
+                                $scope.otkrij={"display":"block"}
                                 $scope.stilEkrana={
-                                    "transition":"1s",
-                                    "filter":"brightness(1)",
-                                    "background":"white"
+                                    "transition":"3s",
+                                    "background":"white",
+                                    "filter":"brightness(1)"
                                 }
-                            },5000)
+                            }
+                        }
+                    }
+                    $scope.okreni=function(broj){
+                        if(broj==1)
+                        $scope.stil111={"transform":"rotate(360deg)","transition":"2s"}
+                        else if(broj==2)
+                        $scope.stil222={"transform":"rotate(360deg)","transition":"2s"}
+
+                        if(broj==11)
+                        {
+                            $timeout(function(){
+                                $scope.stil111=""
+                            },2000)
+                        }
+                        else if(broj==22)
+                        {
+                            $timeout(function(){
+                                $scope.stil222=""
+                            },2000)
                         }
                     }
                 })
